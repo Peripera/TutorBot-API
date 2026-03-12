50de4b01a80deccfa86f7cc20a06ca125eac073b
Peripera <156480421+Peripera@users.noreply.github.com>
2026-03-12 13:08:38 -0600

    Initial commit

---
+++ src/main/java/com/tutorbot/TutorbotApiApplication.java
@@ -0,0 +1,13 @@
+package com.tutorbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorbotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorbotApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorbot/TutorbotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorbotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
